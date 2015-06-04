282c7990d4091682b6daa6aefb7725728a3343e9
SoftFx <andrei.pazniak@soft-fx.lv>
2015-06-04 15:14:24 +0300

    Added Fdk build configuration. Useful to get up-to-date configuration.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-"d:\Work\nant-0.92\bin\NAnt.exe" 
+"d:\Work\nant-0.92\bin\NAnt.exe" build_fdk
 
 @Rem devtools::document(roclets=c('rd', 'collate', 'namespace'))
 @Rem devtools::build(binary = TRUE, args = c('--preclean'))
