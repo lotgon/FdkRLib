40bf299eb89f0e6f2e93d174a6bdbdc8a882ef13
SoftFx <andrei.pazniak@soft-fx.lv>
2015-06-04 15:57:45 +0300

    Added build steps for embedding FDK build steps.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-"d:\Work\nant-0.92\bin\NAnt.exe" build_fdk
+"d:\Work\nant-0.92\bin\NAnt.exe" build_all
 
 @Rem devtools::document(roclets=c('rd', 'collate', 'namespace'))
 @Rem devtools::build(binary = TRUE, args = c('--preclean'))
