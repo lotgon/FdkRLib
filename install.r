5ce990e2815d2a5abb3e8eef2ed01966dffa9c80
SoftFx <andrei.pazniak@soft-fx.lv>
2015-06-08 13:47:55 +0300

    Update install.r version.

--- install.r
+++ install.r
@@ -12,5 +12,5 @@ installBinary <- function(fdkRLibPackage){
 installBinary("rClr_0.7-4.zip")
 library(rClr)
 
-installBinary("FdkRLib_1.0.20150601.zip")
+installBinary("FdkRLib_1.0.20150608.zip")
 library(FdkRLib)
