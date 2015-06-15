321e07b512e02c32d48b114f56538d74c6ae51a3
Ciprian Khlud <ciprian.mustiata@gmail.com>
2015-06-15 19:20:01 +0300

    Code updates

--- R/FdkQuotesL2.r
+++ R/FdkQuotesL2.r
@@ -6,24 +6,33 @@
 #' @param endTime Ending time. Use ttGetEpochFromText if you want to take from text a valid date.
 #' @export
 ttQuotesLevel2 <- function(symbol,startTime, endTime){
-  quotesHistory <- ComputeQuoteLevel2(symbol,startTime, endTime)
+  quotesHistory <- GetQuotePacked(symbol,startTime, endTime)
   
   createTime <- QuotesCreatingTime(quotesHistory)
   UnregisterVar(quotesHistory)
   df = data.frame(ask=ask, bid=bid, createTime=createTime, volume=volume)       
 }
 
+#' Gets the bars' ask as requested
+#' 
+#' @param symbol Symbol looked
+#' @param startTime Starting time. Use ttGetEpochFromText if you want to take from text a valid date.
+#' @param endTime Ending time. Use ttGetEpochFromText if you want to take from text a valid date.
+#' 
+GetQuotePacked <- function(symbol,startTime, endTime) {
+  clrCallStatic('RHost.FdkLevel2', 'GetQuotePacked', symbol,startTime, endTime)
+}
 
 #' Gets the bars' ask as requested
 #' 
 #' @param quotesVar RHost variable that stores quotes array
 QuotesLevel2VolumeBid <- function(quotesVar) {
-  clrCallStatic('RHost.FdkQuotes', 'QuotesVolumeBid', quotesVar)
+  clrCallStatic('RHost.FdkLevel2', 'QuotesVolumeBid', quotesVar)
 }
 
 #' Gets the bars' ask as requested
 #' 
 #' @param quotesVar RHost variable that stores quotes array
 QuotesVolumeAsk <- function(quotesVar) {
-  clrCallStatic('RHost.FdkQuotes', 'QuotesVolumeAsk', quotesVar)
+  clrCallStatic('RHost.FdkLevel2', 'QuotesVolumeAsk', quotesVar)
 }
