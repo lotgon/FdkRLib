ec10f81b81a00cd7ab3e6d70cccd5637c3e2bdd0
Ciprian Khlud <ciprian.mustiata@gmail.com>
2015-06-24 12:04:09 +0300

    Alternate method to install using 'httr' package.

--- install.r
+++ install.r
@@ -8,11 +8,42 @@ installBinary <- function(fdkRLibPackage){
   install.packages(localTempFile, repos = NULL, type = "source")
   file.remove(localTempFile)
 }
+
+installBinaryHttr <- function(fdkRLibPackage){
+  require(httr)
+  basicUrl = "https://github.com/SoftFx/FdkRLib/raw/master/dist/"
+  fullUrl = paste(basicUrl, fdkRLibPackage, sep = "")
+  # this line fails on an clean machine with weird binary internal package error.
+  # localTempFile <-tempfile()
+  localTempFile <- fdkRLibPackage
+  r <-GET(fullUrl)
+  bin <- content(r, "raw")
+  writeBin(bin, localTempFile)
+  
+  install.packages(localTempFile, repos = NULL, type = "source")
+  file.remove(localTempFile)
+}
+
+
+installAllPackages <- function(){
+  setInternet2(TRUE)
+  
+  installBinary("rClr_0.7-4.zip")
+  installBinary("FdkRLib_1.0.20150622.zip")  
+}
+
+#Use this function if you have issues with default 
+# 'installAllPackages' function
+installAllPackagesHttr <- function () {
+  install.packages("httr", repos='http://cran.us.r-project.org')
+  installBinaryHttr("rClr_0.7-4.zip")
+  installBinaryHttr("FdkRLib_1.0.20150622.zip")
+}
+
 # Sets for R environment the command to allow downloading https files
-setInternet2(TRUE)
+installAllPackages()
+# uncomment the next line if you have issues downloading with previous function
+# installAllPackagesHttr()
 
-installBinary("rClr_0.7-4.zip")
 library(rClr)
-
-installBinary("FdkRLib_1.0.20150622.zip")
 library(FdkRLib)
