7249b28595819677ab249812ffdc76331967bacc
Ciprian Khlud <ciprian.mustiata@gmail.com>
2015-06-29 12:00:18 +0300

    Package upgrade of FDK. Fdk is based now on VS 2013.

--- Fdk2R/RSoftFxHost/FdkTradeReports.cs
+++ Fdk2R/RSoftFxHost/FdkTradeReports.cs
@@ -270,5 +270,7 @@ namespace RHost
             var tradeData = FdkVars.GetValue<TradeTransactionReport[]>(varName);
             return tradeData.Select(it => it.TransactionTime).ToArray();
         }
+
+
     }
 }
