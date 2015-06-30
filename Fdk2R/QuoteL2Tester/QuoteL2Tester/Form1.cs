1d5de05dea1319ba13d39d9a0be6a4fb3f87f122
Ciprian Khlud <ciprian.mustiata@gmail.com>
2015-06-30 14:30:36 +0300

    Added original version for testing L2 quotes.

---
+++ Fdk2R/QuoteL2Tester/QuoteL2Tester/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RHost;
+
+namespace QuoteL2Tester
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tbxAddress.Text = MainSetting.Default.Address;
+            tbxLogin.Text = MainSetting.Default.Login;
+            maskedTextBox1.Text = MainSetting.Default.Password;
+            tbxCachesTextBox.Text = MainSetting.Default.CachesPath;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveCredentialsToSettings();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveCredentialsToSettings();
+            var settings = MainSetting.Default;
+            try
+            {
+                FdkHelper.ConnectToFdk(settings.Address, settings.Login, settings.Password, settings.CachesPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("Exception: {0}", ex));
+            }
+        }
+
+        private void SaveCredentialsToSettings()
+        {
+            MainSetting.Default.Address = tbxAddress.Text.Trim();
+            MainSetting.Default.Login = tbxLogin.Text.Trim();
+            MainSetting.Default.Password = maskedTextBox1.Text.Trim();
+            MainSetting.Default.CachesPath = tbxCachesTextBox.Text.Trim();
+
+            MainSetting.Default.Save();
+        }
+    }
+}
+
