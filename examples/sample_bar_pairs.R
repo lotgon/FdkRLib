659c7b9987295d2696a2e6bf8e2adb317366d525
Ciprian Khlud <ciprian.mustiata@gmail.com>
2015-07-13 17:07:34 +0300

    Update sample_bar_pairs.R

--- examples/sample_bar_pairs.R
+++ examples/sample_bar_pairs.R
@@ -3,6 +3,6 @@ startTime <- as.POSIXct(0, origin=ISOdatetime(2015,3,2,11,16,16))
 st1 <- as.POSIXct(startTime)
 et1 <- as.POSIXct(endTime)
 
-barPairs = ttBarsQuotes(symbol = "EURUSD", barPeriodStr = "M1", startTime = st1)
+barPairs = ttBarsQuotesHistory(symbol = "EURUSD", barPeriodStr = "M1", startTime = st1)
 
-barPairsRange = ttBarsQuotes(symbol = "EURUSD", barPeriodStr = "M1", startTime= st1, endTime = et1)
+barPairsRange = ttBarsQuotesHistory(symbol = "EURUSD", barPeriodStr = "M1", startTime= st1, endTime = et1)
