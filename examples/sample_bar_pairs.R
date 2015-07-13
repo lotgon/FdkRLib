65844c63043deff44ef14c069dc2e89e78a02ad0
Ciprian Khlud <ciprian.mustiata@gmail.com>
2015-07-13 16:56:12 +0300

    Update sample_bar_pairs.R

--- examples/sample_bar_pairs.R
+++ examples/sample_bar_pairs.R
@@ -1,5 +1,5 @@
 endTime <- as.POSIXlt(Sys.time())
-startTime <- strptime("20/3/2 11:16:16.683", "%d/%m/%y %H:%M:%OS")
+startTime <- as.POSIXct(0, origin=ISOdatetime(2015,3,2,11,16,16))
 st1 <- as.POSIXct(startTime)
 et1 <- as.POSIXct(endTime)
 
