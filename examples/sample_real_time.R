e9474efb08b6c6fd5376de6b526c25f774946890
Ciprian Khlud <ciprian.mustiata@gmail.com>
2015-07-13 14:05:31 +0300

    Include level in quotes' subscribe

--- examples/sample_real_time.R
+++ examples/sample_real_time.R
@@ -1,5 +1,5 @@
 ttConnect()
-idMonitor <- ttQuotesSubscribe('EURUSD')
+idMonitor <- ttQuotesSubscribe('EURUSD', 2)
 # Wait for some time before running this line
 # it can be run multiple times and the snapshot reflects
 snapshot <- ttQuotesLevel2(idMonitor)
